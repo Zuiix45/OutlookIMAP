e3825b3e57ae72084789ebe2701ecea34a5192e2
Zuiix45 <kesikbas.kerem1@gmail.com>
2024-06-06 17:09:20 +0300

    fix

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 import os
 import fetcher
 
+from dotenv import load_dotenv
+
 USERNAME = os.getenv("EMAIL_ADDRESS")
 PASSWORD = os.getenv("EMAIL_PASSWORD")
 OUTLOOK_IMAP = "outlook.office365.com"
 
 def main():
+    load_dotenv()
+    
     imap = fetcher.login(USERNAME, PASSWORD, OUTLOOK_IMAP)
     
     sender, subject, body = fetcher.fetch_email(1, imap)
